e7607c13d3d0d12a800ccfc8d657c704108f20c5
Assefa Tesfay <assefatesfay@outlook.com>
2020-02-29 07:32:06 -0800

    add asyc and await

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -1,31 +1,27 @@
 import express from 'express';
-import fs from 'fs';
-import { resolve } from 'dns';
-import { rejects } from 'assert';
+import { promises } from 'fs';
+import { raw } from 'body-parser';
 const datafile = 'server/data/clothing.json';
 const router = express.Router();
 
 /* GET all clothing */
 router.route('/')
-  .get(function(req, res) {
-    getClothingData().then(data => res.send(data))
-    .catch(err => res.status(500).send(err));
-    console.log("Doing more work after");
+  .get(async function (req, res) {
+    try {
+      let data = await getClothingData();
+      res.send(data);
+    }
+    catch (error) {
+      res.status(500).send(error);
+    }
+    
   });
 
 
-  function getClothingData(callback) {
-    return new Promise((resolve, reject) => {
-      fs.readFile(datafile, "utf8", (err, data) => {
-        if(err) {
-          reject(err);
-        }
-        else {
-          let clothingData = JSON.parse(data);
-          resolve(clothingData);
-        }
-      });
-    });
-  }
+async function getClothingData() {
+  const rawData = await promises.readFile(datafile, "utf-8");
+  const clothingData = JSON.parse(rawData);
+  return clothingData;
+}
 
 export default router;
